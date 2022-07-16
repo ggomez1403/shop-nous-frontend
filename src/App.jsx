82fdc94f64551f6113ba5f30ea68b741a19bcded
Geraldine Gomez <geralgomez1403@gmail.com>
2022-07-15 21:12:01 -0500

    NOUS Frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+
+const App = () => {
+  return <ProductList />;
+};
+
+export default App;
